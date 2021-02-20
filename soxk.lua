b7c1a6307313c8441a7163f53081103ffdd2344d
oxk666 <78975134+oxk666@users.noreply.github.com>
2021-02-20 16:38:33 +0000

    Update soxk.lua

--- soxk.lua
+++ soxk.lua
@@ -1,22 +1,4 @@
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░                           --------- VOIT LAITTAA KONTENTTI .LUAAN, JOS HALUAT, ETTÄ EI VOI RÄMPYTTÄÄ, OSAAT VARMAAN!?!?!?!?!?
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░                            --- LUOTILIIVIN SIIS
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
-░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
+
 ░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░░A░B░U░?░ ░o░̶░x░̶░k░#░6░6░6░6░
 ESX               = nil
 
@@ -26,4 +8,4 @@ ESX.RegisterUsableItem('luottarit', function(luottari) -- Usable item = Itemi, m
     TriggerClientEvent('esx_luotiliivi:pue', luottari) -- EVENTTI
     local xPlayer = ESX.GetPlayerFromId(luottari)
     local drill = xPlayer.getInventoryItem('luottarit') -- ITEMI ITEMI ITEMI ITEMI ITEMI
-end)
+xPlayer.
